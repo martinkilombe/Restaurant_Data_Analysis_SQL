0ceb785279155dee7c5d7f9ed51c898bbfc42a30
martin kilombe <martinkilombe@gmail.com>
2023-02-18 15:32:55 +0300

    third commit

--- restaurant_data.sql
+++ restaurant_data.sql
@@ -1,4 +1,4 @@
 #Grouping the table according to price of the restaurant
 SELECT * FROM restaurants.restaurants
 ORDER BY price;
-#Selecting restaurants that DO NOT serve beer and allow smoking
+#Selecting restaurants that DO NOT serve beer and allow smoking.
