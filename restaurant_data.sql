1e109184c2d9db4066d7aaf8dec9f89478886269
martin kilombe <martinkilombe@gmail.com>
2023-02-18 20:34:46 +0300

    new changes

--- restaurant_data.sql
+++ restaurant_data.sql
@@ -5,7 +5,18 @@ ORDER BY price;
 SELECT * FROM restaurants.restaurants
 WHERE Alcohol_Service LIKE "%None%" AND  Smoking_Allowed LIKE "%NO%";
 
-#Sql statement that calulates the percentage of hotels that dont servce alcohol
+#calulate the percentage of hotels that DO NOT serve alcohol
 SELECT COUNT(Alcohol_Service) as total_rows,
 SUM(CASE WHEN Alcohol_Service = 'None' THEN 1 ELSE 0 END) AS none_count,(SUM(CASE WHEN Alcohol_Service = 'None' THEN 1 ELSE 0 END) / COUNT(Alcohol_Service)) * 100 as none_percentage
 FROM restaurants.restaurants;
+
+#Calculating using two conditional Expressions for Hotels that both serve alochol and those that DO NOT(None and wine & Beer)
+SELECT COUNT(Alcohol_Service) as total_rows,
+SUM(CASE WHEN Alcohol_Service = 'None' THEN 1 ELSE 0 END) as no_count,
+SUM(CASE WHEN Alcohol_Service = 'Wine & Beer' THEN 1 ELSE 0 END) as yes_count,
+(SUM(CASE WHEN Alcohol_Service = 'None' THEN 1 ELSE 0 END) / COUNT(Alcohol_Service)) * 100 as no_percentage,
+(SUM(CASE WHEN Alcohol_Service = 'Wine & Beer' THEN 1 ELSE 0 END) / COUNT(*)) * 100 as yes_percentage
+FROM restaurants.restaurants;
+
+#calulate the percentage of hotels that ARE franchises and those that are NOT
+
