e064d010f7e1345c99474c26c09e1c756933fe6a
martin kilombe <martinkilombe@gmail.com>
2023-02-18 20:17:02 +0300

    new changes

--- restaurant_data.sql
+++ restaurant_data.sql
@@ -2,3 +2,10 @@
 SELECT * FROM restaurants.restaurants
 ORDER BY price;
 #Selecting restaurants that DO NOT serve beer and allow smoking.
+SELECT * FROM restaurants.restaurants
+WHERE Alcohol_Service LIKE "%None%" AND  Smoking_Allowed LIKE "%NO%";
+
+#Sql statement that calulates the percentage of hotels that dont servce alcohol
+SELECT COUNT(Alcohol_Service) as total_rows,
+SUM(CASE WHEN Alcohol_Service = 'None' THEN 1 ELSE 0 END) AS none_count,(SUM(CASE WHEN Alcohol_Service = 'None' THEN 1 ELSE 0 END) / COUNT(Alcohol_Service)) * 100 as none_percentage
+FROM restaurants.restaurants;
